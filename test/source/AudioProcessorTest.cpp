e4938c86f2a8fc202d992b1d7922ac45e7ff3807
Enrique Mosqueda <enrique7831@gmail.com>
2023-12-04 22:09:27 -0800

    cmake setup for juce, gtest, cpm for init NeuroWave

---
+++ test/source/AudioProcessorTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <NeuroWave/PluginProcessor.h>
+
+namespace audio_plugin_test
+{
+    TEST(AudioPluginAudioProcessor, Foo)
+    {
+        AudioPluginAudioProcessor processor{};
+        ASSERT_FALSE(true);
+    }
+}
